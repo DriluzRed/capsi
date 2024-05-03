5bcf1376623d6ccdd7a9d2372d1ae9895107e6d6
Arturo Gomez <arturorafa74@gmail.com>
2024-05-02 20:23:50 -0400

    fix

--- resources/views/pages/pacientes/index.blade.php
+++ resources/views/pages/pacientes/index.blade.php
@@ -34,27 +34,28 @@
                                     </thead>
                                     <tbody>
                                         
-                                        @foreach($pacientes as $paciente)
+                                        @forelse ($pacientes as $paciente)
                                             <tr>
-                                                @if(count($paciente->userDetalles) > 0)
                                                     <td>{{ $paciente->userDetalles[0]->nombres}}</td>
                                                     <td>{{ $paciente->userDetalles[0]->apellidos }}</td>
                                                     <td>{{ $paciente->userDetalles[0]->edad }}</td>
                                                     <td>{{ $paciente->userDetalles[0]->motivo_consutla }}</td>
-                                                @else
-                                                    <td colspan="3">No hay detalles disponibles para este paciente</td>
-                                                @endif
-                                                <td>
-                                                    <a href="{{ route('pacientes.show', $paciente->id) }}" class="btn btn-primary btn-sm"><i class="fas fa-solid fa-expand"></i> Ver</a>
-                                                    <a href="{{ route('pacientes.edit', $paciente->id) }}" class="btn btn-primary btn-sm"><i class="fas fa-solid fa-pen"></i></a>
-                                                    <form action="{{ route('pacientes.destroy', $paciente->id) }}" method="POST" style="display: inline;">
-                                                        @csrf
-                                                        @method('DELETE')
-                                                        <button type="submit" class="btn btn-danger btn-sm"><i class="fas fa-solid fa-trash"></i></button>
-                                                    </form>
-                                                </td>
+                                                    <td>
+                                                        <a href="{{ route('pacientes.show', $paciente->id) }}" class="btn btn-primary btn-sm"><i class="fas fa-solid fa-expand"></i> Ver</a>
+                                                        <a href="{{ route('pacientes.edit', $paciente->id) }}" class="btn btn-primary btn-sm"><i class="fas fa-solid fa-pen"></i></a>
+                                                        <form action="{{ route('pacientes.destroy', $paciente->id) }}" method="POST" style="display: inline;">
+                                                            @csrf
+                                                            @method('DELETE')
+                                                            <button type="submit" class="btn btn-danger btn-sm"><i class="fas fa-solid fa-trash"></i></button>
+                                                        </form>
+                                                    </td>
                                             </tr>
-                                        @endforeach
+                                        @empty
+                                        <tr>
+                                            <td colspan="3">No hay detalles disponibles para este paciente</td>
+                                        </tr>
+                                        @endforelse
+
                                     </tbody>
                                 </table>
                             </div>
